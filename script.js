6a3da8b52f61a814bc0539416127968eb7d26287
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-03-11 23:26:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 import {Trie} from './Trie.js';
 
 onload = function () {
+const templates = document.getElementsByTagName('template')[0];
+    const contact_item = templates.content.querySelector("div");
+    const add = document.getElementById("add");
+    const contact_info = document.getElementById("contact_input");
+    const del = document.getElementById("del");
+    const delete_info = document.getElementById("delete_input");
+    const info = document.getElementById("info");
+    const contact_list = new Trie();
