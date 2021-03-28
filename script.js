08064471eed1e79f10cca228fdc244f48a34f76c
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-03-28 14:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -159,3 +159,10 @@ const templates = document.getElementsByTagName('template')[0];
                 }
             }
         };
+        
+        /*execute a function when someone clicks in the document:*/
+        document.addEventListener("click", function (e) {
+            closeAllLists(e.target);
+        });
+
+    };
